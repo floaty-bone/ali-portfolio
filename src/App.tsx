bf9140c66448517b62f97b48bed6cf69ab81a605
Ali Abouelazz <ali.abouelazz@gmail.com>
2026-06-01 16:03:17 +0200

    remove unused EducationExperience import to fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import MainPage from './pages/mainPage.tsx'
-import EducationExperience from './pages/experience.tsx'
 import DownloadsPage from './pages/downloadsPage.tsx'
 import CompetencesPage from './pages/competencesPage.tsx'
 import CentreInteret from './pages/loisirs.tsx'
